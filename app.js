1261d13f4a6411b16cafa2c6d6c063c896c9747f
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2023-02-13 14:53:16 +0200

    delete readme

---
+++ app.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to CORS server 😁')
+})
+app.get('/v2/places', (request, response) => {
+    response.header({'Access-Control-Allow-Origin': '*'});
+    axios.get('https://open-api.myhelsinki.fi/v2/places/')
+    .then(res => {
+        response.send(res.data)
+        // console.log(res.data)
+    })
+    .catch(err => {
+        console.log(err)
+    })
+    // response.send({ "msg": "This has CORS enabled 🎈" })
+    // response.json({data: "Hello World"})
+
+    })
+app.listen(8000, () => {
+    console.log('listening on port 8000')
+})
