cb6b24911fcd496bf9e9dfd596ea7faeb9ab5921
Josephine Closan <89244648+JoeClos@users.noreply.github.com>
2023-02-13 15:23:06 +0200

    create a constant for port

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-const axios = require('axios');
-const express = require('express');
+const axios = require("axios");
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
+const port = process.env.PORT || 8000;
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to CORS server 😁')
-})
-app.get('/v2/places', (request, response) => {
-    response.header({'Access-Control-Allow-Origin': '*'});
-    axios.get('https://open-api.myhelsinki.fi/v2/places/')
-    .then(res => {
-        response.send(res.data)
-        // console.log(res.data)
+app.get("/", (req, res) => {
+  res.send("Welcome to CORS server 😁");
+});
+app.get("/v2/places", (request, response) => {
+  response.header({ "Access-Control-Allow-Origin": "*" });
+  axios
+    .get("https://open-api.myhelsinki.fi/v2/places/")
+    .then((res) => {
+      response.send(res.data);
+      // console.log(res.data)
     })
-    .catch(err => {
-        console.log(err)
-    })
-    // response.send({ "msg": "This has CORS enabled 🎈" })
-    // response.json({data: "Hello World"})
-
-    })
-app.listen(8000, () => {
-    console.log('listening on port 8000')
-})
+    .catch((err) => {
+      console.log(err);
+    });
+  // response.send({ "msg": "This has CORS enabled 🎈" })
+  // response.json({data: "Hello World"})
+});
+app.listen(port, () => {
+  console.log(`Now listening on port ${port}...`);
+});
